932072f36cb8bbd87105f10d3679e174154f2597
ebrahim-mohammad <ebrahimmhamd7@gmail.com>
2023-11-04 21:42:34 +0200

    api login

--- FocalApp/config/app.php
+++ FocalApp/config/app.php
@@ -181,6 +181,9 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+        
 
         /*
          * Package Service Providers...
@@ -211,5 +214,7 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
+    'Form' => Collective\Html\FormFacade::class,
+    'Html' => Collective\Html\HtmlFacade::class,
 
 ];
--- FocalApp/database/seeders/UserSeeder.php
+++ FocalApp/database/seeders/UserSeeder.php
@@ -2,11 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -17,11 +15,13 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $user=User::create([
-            'name' => 'admin',
-            'email' => 'admin.@gmail.com',
-            'password' => Hash::make('123456'),
-            'status'=>1
+        DB::table("users")->delete();
+
+        DB::table("users")->insert([
+            'id' =>1,
+            'name'=> 'ebrahim',
+            'email'=> 'ebrahim@gmail.com',
+            'password'=> 'E12345678E'
         ]);
     }
 }
